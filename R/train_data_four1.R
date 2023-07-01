fad630f1b5eb28e2ad90027171f7ec07168a748d
Troy P. Wixson <troy.wixson12@gmail.com>
2023-06-30 22:43:52 -0600

    Cut train_data_four into four smaller files

---
+++ R/train_data_four1.R
@@ -0,0 +1,7 @@
+#' Training data used to train `nnadic` model
+#'
+#' @format ## `train_data_four1`
+#' The first of four arrays with 8000 data sets of 500 bivariate points
+#'
+#' @source data_generation_four_types.R
+"train_data_four1"
